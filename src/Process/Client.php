144bbbc692e601218d4f8ff8192066a8eae6e18d
qiudongfang <apple@ipple.net>
2022-09-17 14:08:25 +0800

    增加审批流部分接口

--- src/Process/Client.php
+++ src/Process/Client.php
@@ -28,6 +28,30 @@ class Client extends BaseClient
     }
 
     /**
+     * 撤销审批实例
+     *
+     * @param array $params
+     * https://open.dingtalk.com/document/orgapp-server/terminate-a-workflow-by-using-an-instance-id
+     * @return mixed
+     */
+    public function terminate($params)
+    {
+        return $this->client->postJson('topapi/process/instance/terminate', ['request' => $params]);
+    }
+
+    /**
+     * 更新审批实例状态
+     *
+     * @param array $params
+     * https://open.dingtalk.com/document/orgapp-server/to-do-instance-status
+     * @return mixed
+     */
+    public function update($params)
+    {
+        return $this->client->postJson('topapi/process/workrecord/update', ['request' => $params]);
+    }
+
+    /**
      * 批量获取审批实例 ID
      *
      * @param array $params
@@ -64,6 +88,30 @@ class Client extends BaseClient
     }
 
     /**
+     * 删除审批模板
+     *
+     * @param array $params
+     *
+     * @return mixed
+     */
+    public function delete($params)
+    {
+        return $this->client->postJson('topapi/process/delete', ['request' => $params]);
+    }
+
+    /**
+     * 获取审批模板code
+     *
+     * @param array $name
+     *
+     * @return mixed
+     */
+    public function getCode($name)
+    {
+        return $this->client->postJson('topapi/process/get_by_name', ['name' => $name]);
+    }
+
+    /**
      * 获取用户待审批数量
      *
      * @param string $userId
@@ -87,5 +135,41 @@ class Client extends BaseClient
     public function listByUserId($userId = null, $offset = 0, $size = 100)
     {
         return $this->client->postJson('topapi/process/listbyuserid', ['userid' => $userId, 'offset' => $offset, 'size' => $size]);
+    }
+
+    /**
+     * 下载审批附件
+     * https://open.dingtalk.com/document/orgapp-server/grants-the-permission-to-download-the-approval-file
+     * @param array $params
+     *
+     * @return mixed
+     */
+    public function fileUrlGet($params)
+    {
+        return $this->client->postJson('topapi/processinstance/file/url/get', ['request' => $params]);
+    }
+
+    /**
+     * 同意或拒绝审批任务
+     * https://open.dingtalk.com/document/orgapp-server/execute-approval-operation-with-attachment
+     * @param array $params
+     *
+     * @return mixed
+     */
+    public function execute($params)
+    {
+        return $this->client->postJson('topapi/process/instance/execute', ['request' => $params]);
+    }
+
+    /**
+     * 添加评论
+     * https://open.dingtalk.com/document/orgapp-server/add-an-approval-comment
+     * @param array $params
+     *
+     * @return mixed
+     */
+    public function commentAdd($params)
+    {
+        return $this->client->postJson('topapi/process/instance/comment/add', ['request' => $params]);
     }
 }
